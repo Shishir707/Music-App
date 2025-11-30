d36f288c71a1dd5089143bda676965de35f0fab1
Shishir707 <mirshr8@gmail.com>
2025-11-30 19:48:16 +0600

    initialize play next previous & toggleButton

--- lib/music_app.dart
+++ lib/music_app.dart
@@ -1,5 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:music_app/model/song.dart';
 
 class MusicApp extends StatefulWidget {
   const MusicApp({super.key});
@@ -10,6 +11,13 @@ class MusicApp extends StatefulWidget {
 
 class _MusicAppState extends State<MusicApp> {
   final AudioPlayer _audioPlayer = AudioPlayer();
+  final List<Song> playList = [];
+
+  int currentIndex = 0;
+  bool isPlay = false;
+
+  Duration songDuration = Duration.zero;
+  Duration position = Duration.zero;
 
   @override
   Widget build(BuildContext context) {
@@ -69,5 +77,34 @@ class _MusicAppState extends State<MusicApp> {
         ],
       ),
     );
+  }
+
+  Future<void> _playSong(int index) async {
+    currentIndex = index;
+    final song = playList[index];
+    setState(() {
+      songDuration = Duration(seconds: song.totalDuration);
+      position = Duration.zero;
+    });
+    await _audioPlayer.stop();
+    await _audioPlayer.play(UrlSource(playList[index].songUrl));
+  }
+
+  Future<void> nextSong() async {
+    final next = (currentIndex + 1) % playList.length;
+    await _playSong(next);
+  }
+
+  Future<void> previousSong() async {
+    final previous = (currentIndex - 1 + playList.length) % playList.length;
+    await _playSong(previous);
+  }
+
+  Future<void> toggleButton() async {
+    if (isPlay) {
+      await _audioPlayer.pause();
+    } else {
+      _audioPlayer.resume();
+    }
   }
 }
