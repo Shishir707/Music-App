23cf8b559bd1573376fa7d483873a3ebf37e58af
Shishir707 <mirshr8@gmail.com>
2025-11-30 19:00:24 +0600

    make a simple UI for app

--- lib/music_app.dart
+++ lib/music_app.dart
@@ -19,6 +19,49 @@ class _MusicAppState extends State<MusicApp> {
         centerTitle: true,
         backgroundColor: Colors.blue.shade200,
       ),
+      body: Column(
+        children: [
+          SizedBox(height: 10),
+          Card(
+            color: Colors.grey[50],
+            child: Column(
+              children: [
+                Text("Song name"),
+                Text('Artist name'),
+                Slider(value: 0, onChanged: (value) {}),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text('Current Time'), Text("Total Duration")],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.skip_previous),
+                    ),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.pause)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.skip_next)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Song name'),
+                  subtitle: Text('Artist name'),
+                  trailing: Icon(Icons.skip_next),
+                  leading: Text("${index + 1}"),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
